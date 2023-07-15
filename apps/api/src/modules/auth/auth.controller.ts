de6b3c1414b8c8e727d28ca036db50e032776b2d
mxrcury <demetrgonchar@gmail.com>
2023-07-15 15:45:38 +0300

    feat: add roles module and user decorator

--- apps/api/src/modules/auth/auth.controller.ts
+++ apps/api/src/modules/auth/auth.controller.ts
@@ -1,5 +1,5 @@
-import { env } from '@configs'
-import { Body, Controller, Post, Res } from '@nestjs/common'
+import { cookieOptions } from '@configs'
+import { Body, Controller, Post, Query, Res } from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
 import { AuthService } from './auth.service'
 import { SignInDto } from './dto/sign-in.dto'
@@ -11,22 +11,15 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('sign-up')
-  async signup(@Body() dto: SignUpDto) {
-    return this.authService.signUp(dto)
+  async signup(@Body() dto: SignUpDto, @Query('token') token?: string) {
+    return this.authService.signUp(dto, token)
   }
 
   @Post('sign-in')
   async signIn(@Body() dto: SignInDto, @Res({ passthrough: true }) res) {
     const token = await this.authService.signIn(dto)
 
-    const cookieOptions = {
-      httpOnly: true,
-      domain: env.CLIENT_DOMAIN,
-      maxAge: 30 * 24 * 3600 * 1000, // 30days
-      secure: true
-    }
-
-    res.cookie('accessToken', token.accessToken, cookieOptions)
+    res.cookie('token', token.accessToken, cookieOptions)
 
     return token
   }
